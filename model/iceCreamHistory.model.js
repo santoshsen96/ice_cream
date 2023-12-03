f6d519db0e78003ca6f717b3a809aa2616eca988
santosh kumar sen <santosh7381070816@gmail.com>
2023-12-03 20:13:50 +0530

    iceCream history model change

--- model/iceCreamHistory.model.js
+++ model/iceCreamHistory.model.js
@@ -1,32 +1,49 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  image: {
-    type: String,
-    default: "",
-  },
-  category: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
-  size: {
-    type: Number,
-  },
-  mrp: {
-    type: Number,
-  },
-  price: {
-    type: Number,
-  },
-  selling_price: {
-    type: Number,
-  },
-  quantity: {
-    type: Number,
-  },
-  date: Number,
+    totalProfit: {
+        type: Number,
+        required: true,
+      },
+      totalBuyAmount: {
+        type: Number,
+        required: true,
+      },
+      subtotal: {
+        type: Number,
+        required: true,
+      },
+      saleDate: {
+        type: Date,
+        default: Date.now,
+      },
+      itemsSold: [
+        {
+          productId: {
+            type: String
+          },
+          productName: {
+            type: String,
+            required: true,
+          },
+          buyPrice: {
+            type: Number,
+            required: true,
+          },
+          sellingPrice: {
+            type: Number,
+            required: true,
+          },
+          quantitySold: {
+            type: Number,
+            required: true,
+          },
+          totalAmount: {
+            type: Number,
+            required: true,
+          },
+        },
+      ],
 });
 
 const iceCreamHistoryModel = mongoose.model("iceCreamHistory", productSchema);
